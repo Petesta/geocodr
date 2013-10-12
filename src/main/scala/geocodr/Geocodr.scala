3f52ea977e40dde841215569f61280d9d6f60ee4
Andrew Berls <andrew.berls@gmail.com>
2013-10-11 17:51:53 -0700

    the homepage sucks

--- src/main/scala/geocodr/Geocodr.scala
+++ src/main/scala/geocodr/Geocodr.scala
@@ -9,13 +9,29 @@ import scala.concurrent._
 import ExecutionContext.Implicits.global
 
 object HelloPlan extends unfiltered.filter.Plan {
-  val response = 
+  val response =
     <html>
       <head>
-        <link href="assets/css/core.css" media="screen" type="text/css" rel="stylesheet"></link>
+        <meta name="viewport" content="width=device-width" />
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet" type="text/css" />
+        <link href="assets/css/core.css" media="screen" type="text/css" rel="stylesheet" />
       </head>
       <body>
-        Hello World!
+        <div class="container">
+          <div class="landing-container">
+            <div class="cta-container">
+              <h1>geocodr</h1>
+              <p>Connect with developers like you in your area.</p>
+            </div>
+
+            <div class="login-container">
+              <form action="">
+                <input type="text" placeholder="Enter your GitHub username"/>
+                <input type="submit" class="btn btn-large btn-blue" value="Go"/>
+              </form>
+            </div>
+          </div>
+        </div>
       </body>
     </html>
 
