153e2e6dabe5d73ea9615350a5bb3632b1720339
Jared Roesch <roeschinc@gmail.com>
2013-10-12 00:17:53 -0700

    commit JSON

---
+++ web/js/response.json.js
@@ -0,0 +1,14 @@
+{
+    "users": [
+    {
+        "name": "Tristan",
+        "gravtar_url": "l337cats.jpg",
+        "distance": 1337,
+    },
+    {
+        "name": "Jared",
+        "gravtar_url": "wat.jpg",
+        "distance": 10,
+    }
+    ]
+}
