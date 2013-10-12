f3b1f8946c9be75410b39f98b63c417899bf5804
Petesta <Petesta@live.com>
2013-10-11 21:21:44 -0700

    Add case class RepositoryCount for Users

--- src/main/scala/geocodr/github/search/Users.scala
+++ src/main/scala/geocodr/github/search/Users.scala
@@ -3,39 +3,13 @@ package geocodr.github.search
 import dispatch._
 import Defaults._
 
-object User {
-  case class Size(sc: SizeConstraint) extends UserSearchQuery {
-    def query = ???
+object Users {
+  case class RepositoryCount(sc: SizeConstraint) extends UserSearchQuery {
+    def query = s"repos:${sc.filter}"
   }
 
-  case class Forks(sc: SizeConstraint) extends UserSearchQuery {
-    def query = ???
-  }
-
-  //case class Fork()
   case class Created() extends UserSearchQuery {
     def query = ???
-  }
-
-  case class Pushed() extends UserSearchQuery {
-    def query = ???
-  }
-
-  case class User() extends UserSearchQuery {
-    def query = ???
-  }
-
-  case class Language(name: String) extends UserSearchQuery {
-    override def query = s"language:${name.map(_.toLower).mkString("")}"
-  }
-
-  object Followers {
-    def ==(x: Int) = Followers(Eq(x))
-    def <(x: Int) = Followers(LessThan(x))
-    def <=(x: Int) = Followers(LessThanEq(x))
-    def >(x: Int)  = Followers(GreaterThan(x))
-    def >=(x: Int) = Followers(GreaterThanEq(x))
-    def range(pair: (Int, Int)) = Followers(Range(pair._1, pair._2))
   }
 
   case class Followers(sc: SizeConstraint) extends UserSearchQuery {
@@ -43,23 +17,23 @@ object User {
   }
 
   /* SearchSort */
-  sealed trait SearchSort {
+  sealed trait UsersSearchSort {
     def sort: String
   }
 
-  case object SortByFollowers extends SearchSort {
+  case object SortByFollowers extends UsersSearchSort {
     def sort = "followers"
   }
 
-  case object SortByRepositories extends SearchSort {
+  case object SortByRepositories extends UsersSearchSort {
     def sort = "repositories"
   }
 
-  case object SortByJoined extends SearchSort {
+  case object SortByJoined extends UsersSearchSort {
     def sort = "joined"
   }  
 
-  def search(sq: CompoundQuery[UserSearchQuery], s: SearchSort, o: Order) = {
+  def search(sq: CompoundQuery[UserSearchQuery], s: UsersSearchSort, o: Order) = {
     val url = root / "search" / "repositories"
     val params = Map("q" -> sq.query, "sort" -> s.sort, "order" -> o.order) 
     Http(url <:< Seq("Accept" -> "application/vnd.github.preview") <<? params OK as.String)
